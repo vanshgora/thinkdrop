7c2eb70556be0b0707dcced38e4355ec4a1b676b
vanshgora <vanshgora31@gmail.com>
2025-07-11 10:57:14 +0530

    forced node process for being keep alive

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const { sendMail } = require("./send-mail");
 
 dotenv.config();
 
-console.log(111);
-
-const task = cron.schedule("0 50 10 * * *", async () => {
+const task = cron.schedule("0 0 11 * * *", async () => {
 	try {
 		const response = await generateNewTask();
 		sendMail("vanshgora30@gmail.com", response.subject, response.content);
